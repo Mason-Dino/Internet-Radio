4de940d6e7f58506a70398198248d67b5fa64465
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-02-17 13:16:19 -0600

    add basic flask webserver

--- radio.py
+++ radio.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 MUSIC = "/home/dsu/Desktop/music"
 playlist = []
 
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
