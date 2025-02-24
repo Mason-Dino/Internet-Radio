a1f5fb426ea6d2a052fc0abcd75e8cbbae378f21
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-02-24 14:13:49 -0600

    add advance code and update readme

---
+++ advanced/radio.py
@@ -0,0 +1,34 @@
+from flask import Flask, Response, render_template
+import os
+
+app = Flask(__name__)
+MUSIC = "/home/dsu/Desktop/music"
+playlist = []
+
+for file in os.listdir(MUSIC):
+    if file.endswith(".mp3"):
+        playlist.append(file)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/list")
+def list():
+    return playlist
+
+def generate(song):
+    song = os.path.join(MUSIC, song)
+    print(song)
+    with open(song, "rb") as f:
+        data = f.read()
+        while data:
+            yield data
+            data = f.read()
+            
+@app.route("/song/<song>")
+def song(song):
+    return Response(generate(song), mimetype="audio/mpeg")
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
